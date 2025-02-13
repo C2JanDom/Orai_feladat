936c36963ed4076c1266074643600cf645fb1fb4
C2JanDom <c2jandom@vasvari.org>
2025-02-13 11:27:35 +0100

    napok száma def

--- feladat.py
+++ feladat.py
@@ -1,10 +1,19 @@
 f = open('vasarlas.csv', 'r')
-napokSzama=0
+nullaKoltes=0
 
-for szam in f:
-    szam = szam.strip()
-    tmp = szam.split(";")
+def szamok():
 
-    print(tmp)
+    for szam in f:
+        szam = szam.strip()
+        tmp = szam.split(";")
+
+def napok(tmp):
+    napokSzama=0
+    for nap in tmp:
+        napokSzama += 1
+
+        print(nap)
+
 
 f.close()
+print("Ennyi nap volt a hónapba: ")
